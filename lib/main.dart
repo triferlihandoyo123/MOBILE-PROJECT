04004ff5788e6661bdef45408a9cd68658a760ce
Indahku@29 <tri_ferli_handoyo@teknokrat.ac.id>
2023-04-11 01:08:37 +0700

    Baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:loginform/register_form.dart';
-
 import 'login_form.dart';
 import 'package:flutter/material.dart';
 import 'contoh_scroll.dart';
